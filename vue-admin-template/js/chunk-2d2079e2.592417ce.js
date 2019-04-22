142aff541a5697fc4a88fa78526b526c7f7c7d7f
515491847 <515491847@qq.com>
2019-04-22 14:54:55 +0800

    Add vue-admin-template

---
+++ vue-admin-template/js/chunk-2d2079e2.592417ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2079e2"],{a0fc:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n  main\n")])},u=[],a=t("2877"),l={},i=Object(a["a"])(l,c,u,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d2079e2.592417ce.js.map
